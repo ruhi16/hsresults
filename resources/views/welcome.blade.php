db89919e05609ef8265a8af820c44584f06bcf31
Hari Narayan Das <hndas16@gmail.com>
2020-05-22 00:28:30 +0530

    heroku changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel <br/>Hari Narayan Das
                 </div>
 
                 
