7122bf0ef86a5c2f08e6ea433a6bc79faa6591ab
Hari Narayan Das <hndas16@gmail.com>
2021-01-10 18:49:03 +0530

    ind member updated

--- app/Http/Controllers/Api/Auth/MemberController.php
+++ app/Http/Controllers/Api/Auth/MemberController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 
 use App\Member;
+use App\Loanassign;
 
 class MemberController extends Controller{
         
@@ -33,40 +34,38 @@ class MemberController extends Controller{
     public function show($id){
         $member = Member::find($id);
 
+        $loanassigns = $member->loanassigns; //Loanassign::where('member_id', $id)->get();
+        
+
+        $payments = [];
+
+        foreach($loanassigns as $loanassign){
+            $payments = $loanassign->loanpayments;
+
+        }
+        
+        $thfundstatus = $member->thfundmasters;
+        $thfundcollection = $member->thfundmonthcollections;
+
         return response()->json([
-            'member' => $member
+            'member' => $member,
+            
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         //
